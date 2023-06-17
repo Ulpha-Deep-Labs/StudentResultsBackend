6cc151e8abbb337aa8941bd27aa47d4e87143ded
Chukwuma Ebube <chukwuma619@gmail.com>
2023-06-17 21:41:04 +0100

    create a score form view

--- portal/results/forms.py
+++ portal/results/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
+import models
 
 from . import models
 
 class StudentLoginForm(forms.Form):
     reg_no = forms.IntegerField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class ScoreForm(forms.ModelForm):
+    class Meta:
+        models = models.CourseItem
+        fields = ['student', 'course', 'student_course_ca', 'student_course_exam_score']
 
--- portal/results/views.py
+++ portal/results/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import StudentLoginForm
-
+from .forms import StudentLoginForm, AddResultForm, SelectCourseForm, ScoreForm
+from django.forms import formset_factory
 from django.contrib.auth.decorators import login_required
 from . import models
 from django.contrib import messages
@@ -20,6 +20,30 @@ def dashboard(request):
 
     return render(request, 'results/dashboard.html', context)
 
+@login_required
+def score_form_view(request, course_id):
+    ScoreFormSet = formset_factory(ScoreForm, extra=0)
+    course = models.Course.objects.get(id=course_id)
+    students = models.Student.objects.filter(courseitem__course=course).order_by('department')
+
+    if request.method == "POST":
+        formset = ScoreFormSet(request.POST)
+
+        if formset.is_valid():
+            formset.save()
+            return redirect("sucess.html")
+        
+    else:
+        initial_data = [{'student': student, 
+                         'CA Score': student.courseitem__student_course_ca,
+                         'Exam Score': student.courseitem__student_course_exam_score,
+                         } for student in students]
+        formset = ScoreFormSet(initial_data=initial_data)
+        context = {
+            "formset": formset,
+            'course': course,
+        }
+    return render(request, 'score_form.html', context)
 
 
 
